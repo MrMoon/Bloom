0cdbc756c5dc0ec372759be8276ee050a0ede92b
mrmoon <abuamaramfreelance@gmail.com>
2021-01-10 22:30:35 +0200

    commit clean up

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -13,12 +13,8 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
-import { TodoComponent } from './apps/todo-list/todo/todo.component';
-import { TodoRtlComponent } from './apps/todo-list/todo-rtl/todo-rtl.component';
-import { TodoListComponent } from './apps/todo-list/todo-list.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 import { ContentAnimateDirective } from './shared/directives/content-animate.directive';
 import {NgSelectModule} from '@ng-select/ng-select';
@@ -32,7 +28,6 @@ import { PatientStatusComponent } from './patient-status/patient-status.componen
 import { DoctorPatientComponent } from './doctor-patient/doctor-patient.component';
 import { StatusComponent } from './status/status.component';
 
-// AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/');
 }
@@ -41,10 +36,6 @@ export function HttpLoaderFactory(http: HttpClient) {
   declarations: [
     AppComponent,
     NavbarComponent,
-    DashboardComponent,
-    TodoComponent,
-    TodoListComponent,
-    TodoRtlComponent,
     SpinnerComponent,
     ContentAnimateDirective,
     EmployeeComponent,
