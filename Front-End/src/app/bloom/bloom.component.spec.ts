d6ac77aa9629ee240631ba2c47043f7709b56a76
mrmoon <abuamaramfreelance@gmail.com>
2021-01-10 15:14:57 +0200

    commit initiate the bloom component

---
+++ Front-End/src/app/bloom/bloom.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BloomComponent } from './bloom.component';
+
+describe('BloomComponent', () => {
+  let component: BloomComponent;
+  let fixture: ComponentFixture<BloomComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BloomComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BloomComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
