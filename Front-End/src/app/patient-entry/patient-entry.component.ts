c748cd745b2ca6a1ca77c8ec5a3f2cd87c71c4a7
mrmoon <abuamaramfreelance@gmail.com>
2021-01-14 00:29:45 +0200

    commit patient-entry components

--- Front-End/src/app/patient-entry/patient-entry.component.ts
+++ Front-End/src/app/patient-entry/patient-entry.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PatientEntry} from '../model/PatientEntry';
+import {PatientEntryService} from '../patient-entry.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-patient-entry',
@@ -8,12 +10,25 @@ import {PatientEntry} from '../model/PatientEntry';
 })
 export class PatientEntryComponent implements OnInit {
 
-  patientEntry: PatientEntry;
+  patientEntry: PatientEntry = new PatientEntry();
 
-  constructor() {
+  constructor(private patientEntryService: PatientEntryService, private toast: ToastrService) {
   }
 
   ngOnInit(): void {
   }
 
+  onPatientEntrySubmit = () => {
+    if (this.patientEntry.patientEntryId !== undefined) {
+      this.patientEntryService.updatePatientEntry(this.patientEntry).subscribe(updatedPatientEntry => {
+        this.patientEntry = updatedPatientEntry;
+        this.toast.success('Patient Entry Updated Successfully', 'Patient Entry  ' + this.patientEntry.patientEntryId + ' Status');
+      }, error => this.toast.error('Patient Entry Update Failed\n' + error, 'Patient Entry Update Status'));
+    } else {
+      this.patientEntryService.createPatientEntry(this.patientEntry).subscribe(savedPatientEntry => {
+        this.patientEntry = savedPatientEntry;
+        this.toast.success('Patient Entry Created Successfully', 'Patient Entry  ' + this.patientEntry.patientEntryId + ' Status');
+      }, error => this.toast.error('Patient Entry Creation Failed\n' + error, 'Patient Entry Creation Status'));
+    }
+  }
 }
