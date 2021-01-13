84e4540faba8b8fcbb87aa669a055da704be1fdc
mrmoon <abuamaramfreelance@gmail.com>
2021-01-13 20:09:12 +0200

    commit inventory crud, join, and aggregation operations

---
+++ Front-End/src/app/inventory.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Inventory} from './model/Inventory';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+
+  private inventoryURL = 'http://localhost:8080/hospital/inventory/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getInventoryById(nurseId: number, inventoryId: number): Observable<Inventory> {
+    return this.http.get<Inventory>(this.inventoryURL + inventoryId + '/' + nurseId);
+  }
+
+  createInventory(inventory: Inventory): Observable<Inventory> {
+    return this.http.post<Inventory>(this.inventoryURL, inventory);
+  }
+
+  updateInventory(inventory: Inventory): Observable<Inventory> {
+    return this.http.put<Inventory>(this.inventoryURL, inventory);
+  }
+
+  deleteInventory(nurseId: number, inventoryId: number): Observable<HttpResponse<any>> {
+    return this.http.delete(this.inventoryURL + inventoryId + '/' + nurseId, {observe: 'response'});
+  }
+}
