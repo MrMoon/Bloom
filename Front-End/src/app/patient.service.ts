a06593f7df9e1d308fae2cb22c6af9d7b3997af1
mrmoon <abuamaramfreelance@gmail.com>
2021-01-13 20:19:04 +0200

    commit update patient-join crud, join, and aggregation operations

--- Front-End/src/app/patient.service.ts
+++ Front-End/src/app/patient.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Patient} from './model/Patient';
+import {PatientRoomDetails} from './model/PatientRoomDetails';
+import {Doctor} from './model/Doctor';
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +29,14 @@ export class PatientService {
 
   deletePatient(patientId: number): Observable<HttpResponse<any>> {
     return this.http.delete(this.patientURL + patientId, {observe: 'response'});
+  }
+
+
+  getPatientRoomDetails(patientId: number): Observable<PatientRoomDetails> {
+    return this.http.get<PatientRoomDetails>(this.patientURL + 'room/' + patientId);
+  }
+
+  getPatientDoctor(patientId: number): Observable<Doctor> {
+    return this.http.get<Doctor>(this.patientURL + 'doctor/' + patientId);
   }
 }
